6653bfce69c8a815342f16f29caae646c198b85a
Olawale Sunmonu-Adedeji <olawalesunmonu-adedeji@Olawales-MacBook-Pro.local>
2020-02-18 11:52:14 +0100

    Higher Order Components

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,13 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from 'actions';
+import requireAuth from 'components/requireAuth';
 class CommentBox extends Component{
     state = {
         comment: ''
+
     }
+    
     handleChange = (event) => {
         this.setState({
             comment: event.target.value
@@ -38,4 +41,7 @@ class CommentBox extends Component{
     }
 }
 
-export default connect(null,actions)(CommentBox);
+
+
+
+export default connect(null,actions)(requireAuth(CommentBox));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter,Route} from 'react-router-dom';
 import App from 'components/App';
 import Root from 'Root';
 
@@ -8,6 +8,8 @@ import Root from 'Root';
 
 ReactDOM.render(
 <Root>
-    <App/>
+    <BrowserRouter>
+        <Route path ="/" component={App} />
+    </BrowserRouter>
 </Root>
 ,document.querySelector('#root'))
